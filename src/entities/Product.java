6209469a4945773ca2767662765a2c0404e5502a
Oygods <lucas.luca2ft@gmail.com>
2022-08-29 19:40:02 -0300

    finalizando o programa

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -8,11 +8,22 @@ public class Product {
     public double totalValueInStock() {
         return price * quantity;
     }
+
     public void addProducts(int quantity) {
         this.quantity += quantity;
     }
 
     public void removeProducts(int quantity) {
         this.quantity -= quantity;
+    }
+
+    public String toString() {
+        return name
+                + ", $ "
+                + String.format("%.2f", price)
+                +  ", "
+                + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
     }
 }
