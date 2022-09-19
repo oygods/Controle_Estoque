3081f6d2babb061d6efb05f393fa231f5ec5bb19
Oygods <lucas.luca2ft@gmail.com>
2022-09-19 16:18:30 -0300

    adicionando sobrecarga

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,17 +18,14 @@ public class Program {
         System.out.print("Price: ");
         double price = sc.nextDouble();
 
-        System.out.print("Quantity in stock: ");
-        int quantity = sc.nextInt();
-
-        Product product = new Product(name, price, quantity);
+        Product product = new Product(name, price);
 
         System.out.println();
         System.out.println("Product data: " + product);
 
         System.out.println();
         System.out.println("Enter the number of products to be added in stock: ");
-        quantity = sc.nextInt();
+        int quantity = sc.nextInt();
         product.addProducts(quantity); //atualizando a qtd dentro do objeto products
 
         System.out.println();
