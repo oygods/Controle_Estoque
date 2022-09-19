6ca4f169b5bb575557f0668edfa91e30c3fcd98e
Oygods <lucas.luca2ft@gmail.com>
2022-09-19 17:06:23 -0300

    adicionando encapsulamento nos atributos

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -1,9 +1,12 @@
 package entities;
 
 public class Product {
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product() {
+    }
 
     public Product(String name, double price, int quantity) {
         this.name = name;
@@ -13,6 +16,22 @@ public class Product {
 
     public Product(String name, double price) {
         this.name = name;
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 
